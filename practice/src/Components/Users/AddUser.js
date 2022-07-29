0bd58cdc94e782b8b61d3aaca9041a52e0d4d701
Mount-Blanc <mountblanc87@gmail.com>
2022-07-29 15:42:13 -0700

    added form to adduser

--- practice/src/Components/Users/AddUser.js
+++ practice/src/Components/Users/AddUser.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const Adduser= props => {};
+const Adduser= props => {
+    return (
+        <form>
+            <label htmlFor='username'>Username</label>
+            <input id='username' type='text'></input>
+            <label htmlFor="age">Age</label>
+            <input id='age' type='number'></input>
+            <button type="submit">Add User</button>
+        </form>
+    );
+
+
+
+};
+
+
 
 export default Adduser;
