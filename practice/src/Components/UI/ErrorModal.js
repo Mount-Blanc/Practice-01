c6c81de0f17bc9a827d83391e017d55df0b113a1
Mount-Blanc <mountblanc87@gmail.com>
2022-07-30 13:46:53 -0700

    added errormodal js file

---
+++ practice/src/Components/UI/ErrorModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+const ErrorModal = (props) => {
+    return ( 
+        <Card>
+            <header>
+                <h2></h2>
+            </header>
+            <div>
+                <p></p>
+            </div>
+            <footer>
+                <button>Okay</button>
+            </footer>
+        </Card>
+        
+    )
+};
+
+export default ErrorModal;
